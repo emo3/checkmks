741941dcf243ce9957c8e25cc802c34fc17ab34e
Ed Overton <e.overton@gmail.com>
2019-08-29 22:38:22 -0400

    add executes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright:: 2019, Ed Overton, Apache 2.0
+package %w(xinetd openssl python epel-release)
+
+execute 'get checkmk' do
+  command 'wget https://checkmk.com/support/1.5.0p21/check-mk-raw-1.5.0p21-el7-38.x86_64.rpm'
+  not_if { File.exist?('/check-mk-raw-1.5.0p21-el7-38.x86_64.rpm') }
+end
+
+execute 'install checkmk' do
+  command 'yum -y install check-mk-raw-1.5.0p21-el7-38.x86_64.rpm'
+  not_if { File.exist?('/opt/omd/sites') }
+end
+
+# execute 'create_sandbox' do
+#   command 'omd create sandbox'
+#   not_if { File.exist?('/opt/omd/sites/sandbox') }
+# end
+#
+# execute 'start sandbox' do
+#   command 'omd start snadbox'
+#   not_if ('ps -f | grep -v grep | grep sandbox')
+#   returns [0, 1, 2]
+# end
+#
+# execute 'change passwd' do
+#   command "su - sandbox -c 'htpasswd -m ~/etc/htpasswd cmkadmin -b cmkadmin'"
+#   command '/usr/sbin/setsebool -P httpd_can_network_connect=1​'
+# end
