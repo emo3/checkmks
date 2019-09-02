507f96a3ea9a36570bd128601660f5163419d65a
Ed Overton <e.overton@gmail.com>
2019-09-01 23:03:04 -0400

    many changes

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,9 +5,7 @@
 # Copyright:: 2019, Ed Overton, Apache 2.0
 # package %w(xinetd openssl python)
 
-epel_local_repo 'my epel' do
-  not_if { File.exist?('/etc/yum.repo.d/epel7.repo') }
-end
+epel_local_repo 'checkmk'
 
 # Download the check_mk raw server package file
 remote_file "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_rpm']}" do
@@ -17,26 +15,32 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_rpm']}" do
 end
 
 package node['cmk']['server_rpm'] do
- source "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_rpm']}"
+  source "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_rpm']}"
 end
 
-# execute 'create_sandbox' do
-#   command 'omd create sandbox'
-#   not_if { File.exist?('/opt/omd/sites/sandbox') }
+execute "create_#{node['cmk']['instance_name']}" do
+  command "omd create --admin-password #{node['cmk']['admin_passwd']} #{node['cmk']['instance_name']}"
+  sensitive true
+  not_if { File.exist?("/opt/omd/sites/#{node['cmk']['instance_name']}") }
+end
+
+## TODO
+# service "apache" do
+#   supports :status => true, :restart => true, :start => true
+#   action [ :start ]
 # end
-#
-# execute 'start sandbox' do
-#   command 'omd start sandbox'
-#   not_if ('ps -eaf | grep -v grep | grep sandbox')
-#   returns [0, 1, 2]
-# end
-#
+
+execute "start_#{node['cmk']['instance_name']}" do
+  command "omd start #{node['cmk']['instance_name']}"
+  not_if ("ps -eaf | grep -v grep | grep #{node['cmk']['instance_name']}")
+end
+
 # execute 'change passwd' do
 #   command "su - sandbox -c 'htpasswd -b -m ~/etc/htpasswd cmkadmin cmkadmin'"
 #   # returns [0, 1, 2]
 # end
-#
-# execute 'fix for selinux' do
-#   command '/usr/sbin/setsebool -P httpd_can_network_connect=1'
-#   # returns [0, 1, 2]
-# end
+
+execute 'fix for selinux' do
+  command '/usr/sbin/setsebool -P httpd_can_network_connect=1'
+  only_if ('/usr/sbin/getsebool httpd_can_network_connect | grep off')
+end
