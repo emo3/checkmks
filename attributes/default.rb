0db02d9b9b5cd5c82c22cee0249817189b3e4e41
Ed Overton <e.overton@gmail.com>
2019-09-01 23:02:04 -0400

    add instance and pass

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['cmk']['media_url']  = 'http://10.1.1.30/media'
+default['cmk']['media_url'] = 'http://10.1.1.30/media'
 default['cmk']['server_rpm'] = 'check-mk-raw-1.5.0p21-el7-38.x86_64.rpm'
+default['cmk']['instance_name'] = 'sandbox'
+default['cmk']['admin_passwd'] = 'cmkadmin'
