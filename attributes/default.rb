4b299cd58189db02d4ac5d4af63f471db7db46b1
Ed Overton <e.overton@gmail.com>
2019-11-11 21:30:31 -0500

    fix passing in server ip as ENV variable

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['cmk']['media_url'] = 'http://10.1.1.30/media'
 default['cmk']['server_rpm'] = 'check-mk-raw-1.5.0p12-el7-38.x86_64.rpm'
 default['cmk']['instance_name'] = 'sandbox'
 default['cmk']['admin_passwd'] = 'cmkadmin'
+default['cmk']['server_ip'] = node['server_ip']
