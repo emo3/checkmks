113793cb2a2443cbce0728b6ed6f009af3139d4e
Ed Overton <e.overton@gmail.com>
2019-12-01 20:58:08 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Ed Overton'
 maintainer_email 'bademail@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures checkmk'
-version          '1.2.0'
+version          '1.3.0'
 chef_version     '>= 14.0' if respond_to?(:chef_version)
 
 supports 'redhat'
