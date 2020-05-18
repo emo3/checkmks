d78814414b55a7d05fc07d787c1cef70c05cb4bb
Ed Overton <e.overton@gmail.com>
2020-05-18 13:19:18 -0400

    commit to point 20200518

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,8 +10,9 @@ name 'checkmk'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'checkmk::server', 'checkmk::add_host'
-#run_list 'checkmk::server'
+#run_list 'checkmk::server', 'checkmk::add_host'
+run_list 'checkmk::server'
 
 # Specify a custom source for a single cookbook:
 cookbook 'checkmk', path: '.'
+cookbook 'server_utils', path: '../server_utils'
