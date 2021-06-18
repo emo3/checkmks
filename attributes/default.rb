463c06fc9e7d96887bef6f08d8245e4975d1badb
Ed Overton <e.overton@gmail.com>
2021-06-18 14:36:30 -0400

    Tidy up variables allow for either 1.6 or 2.0 check mk server to be installed default is 1.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,13 @@
 default['cmk'].tap do |cmk|
   cmk['media_url'] = 'http://websrv/media'
-  cmk['cmk_release'] = '2.0.0'
-  cmk['cmk_version'] = 'p5'
-  # cmk['cmk_release'] = '1.6.0'
-  # cmk['cmk_version'] = 'p24'
+  cmk['cmk_release1'] = '1.6.0p24'
+  cmk['cmk_release2'] = '2.0.0p5'
   cmk['el_version'] = if node['platform_version'] < '8'
                         '7'
                       else
                         '8'
                       end
-  cmk['server_rpm'] = "check-mk-raw-#{cmk['cmk_release']}#{cmk['cmk_version']}-el#{cmk['el_version']}-38.x86_64.rpm"
+  cmk['server_rpm'] = "check-mk-raw-#{cmk['cmk_release1']}-el#{cmk['el_version']}-38.x86_64.rpm"
   cmk['instance_name'] = 'cmk'
   cmk['server_name'] = 'checkmks'
   cmk['admin_passwd'] = 'cmkadmin'
