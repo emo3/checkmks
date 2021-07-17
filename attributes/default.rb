bb05aa1ea5332e2284d72a43fa5337ab771abbd1
Ed Overton <e.overton@gmail.com>
2021-07-17 04:14:12 -0400

    set to use enterprise and p18

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['cmk'].tap do |cmk|
   cmk['media_url'] = 'http://websrv/media'
-  cmk['cmk_release'] = '2.0.0p7'
-  # cmk['cmk_release'] = '1.6.0p24'
+  # cmk['cmk_release'] = '2.0.0p7'
+  cmk['cmk_release'] = '1.6.0p18'
   cmk['el_version'] = if node['platform_version'] < '8'
                         '7'
                       else
@@ -9,13 +9,13 @@ default['cmk'].tap do |cmk|
                       end
   # cmk['server_rpm'] = "check-mk-raw-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
   # 1.6.0 version
-  # cmk['server_rpm'] = "check-mk-enterprise-#{cmk['cmk_release']}.demo-el#{cmk['el_version']}-38.x86_64.rpm"
+  cmk['server_rpm'] = "check-mk-enterprise-#{cmk['cmk_release']}.demo-el#{cmk['el_version']}-38.x86_64.rpm"
   # 2.0.0 version
-  cmk['server_rpm'] = "check-mk-free-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
+  # cmk['server_rpm'] = "check-mk-free-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
   cmk['agent_rpm'] = "check-mk-agent-#{cmk['cmk_release']}-1.noarch.rpm"
   cmk['site_name'] = 'cmk'
   cmk['server_name'] = 'checkmks'
-  cmk['server_ip'] = '192.168.86.130'
+  cmk['server_ip'] = '1.2.3.4'
   cmk['admin_passwd'] = 'cmkadmin'
   cmk['server_ip'] = node['server_ip']
   cmk['agent_ip'] = node['agent_ip']
