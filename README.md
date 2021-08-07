f24da6e9adebba079ae98dcfa1cae7045afee196
Ed Overton <e.overton@gmail.com>
2021-08-07 01:07:19 -0400

    This will allow one to specify both and private IP address for both the CheckMK Server and CheckMK Agent

--- README.md
+++ README.md
@@ -8,14 +8,13 @@ Here is an example of where to get the free version:<br />
 This for RHEL/CentOS only<br />
 ```
 osv=8 # Version of RHEL/CentOS
-cmkv='2.0.0p5' # Version of checkmk server
-wget -q https://download.checkmk.com/checkmk/${cmkv}/check-mk-raw-${cmkv}-el${osv}-38.x86_64.rpm
+cmkv='1.6.0p25' # 1.6 Version of checkmk server
+#cmkv='2.0.0p8' # 2.0 Version of checkmk server
 ## 1.6.0
 wget -q https://download.checkmk.com/checkmk/${cmkv}/check-mk-enterprise-${cmkv}.demo-el${osv}-38.x86_64.rpm
 ## 2.0.0
 wget -q https://download.checkmk.com/checkmk/${cmkv}/check-mk-free-${cmkv}-el${osv}-38.x86_64.rpm
 ```
-
 My local server to get pre-packaged agent
 ```
 wget http://checkmks/cmk/check_mk/agents/check-mk-agent-${cmkv}-1.noarch.rpm
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,12 @@
 default['cmk'].tap do |cmk|
   cmk['media_url'] = 'http://websrv/media'
-  # cmk['cmk_release'] = '2.0.0p7'
-  cmk['cmk_release'] = '1.6.0p18'
+  # cmk['cmk_release'] = '2.0.0p8'
+  cmk['cmk_release'] = '1.6.0p25'
   cmk['el_version'] = if node['platform_version'] < '8'
                         '7'
                       else
                         '8'
                       end
-  # cmk['server_rpm'] = "check-mk-raw-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
   # 1.6.0 version
   cmk['server_rpm'] = "check-mk-enterprise-#{cmk['cmk_release']}.demo-el#{cmk['el_version']}-38.x86_64.rpm"
   # 2.0.0 version
@@ -15,7 +14,6 @@ default['cmk'].tap do |cmk|
   cmk['agent_rpm'] = "check-mk-agent-#{cmk['cmk_release']}-1.noarch.rpm"
   cmk['site_name'] = 'cmk'
   cmk['server_name'] = 'checkmks'
-  cmk['server_ip'] = '1.2.3.4'
   cmk['admin_passwd'] = 'cmkadmin'
   cmk['server_ip'] = node['server_ip']
   cmk['agent_ip'] = node['agent_ip']
