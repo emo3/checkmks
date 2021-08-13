54053a23996138e124ecca4560dd6176e6909050
Ed Overton <e.overton@gmail.com>
2021-08-12 22:56:52 -0400

    Build by default of enterprise 2.0.0p8
    Bump version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,16 @@
 default['cmk'].tap do |cmk|
   cmk['media_url'] = 'http://websrv/media'
-  # cmk['cmk_release'] = '2.0.0p8'
-  cmk['cmk_release'] = '1.6.0p25'
+  cmk['cmk_release'] = '2.0.0p8'
+  # cmk['cmk_release'] = '1.6.0p25'
   cmk['el_version'] = if node['platform_version'] < '8'
                         '7'
                       else
                         '8'
                       end
   # 1.6.0 version
-  cmk['server_rpm'] = "check-mk-enterprise-#{cmk['cmk_release']}.demo-el#{cmk['el_version']}-38.x86_64.rpm"
+  # cmk['server_rpm'] = "check-mk-enterprise-#{cmk['cmk_release']}.demo-el#{cmk['el_version']}-38.x86_64.rpm"
   # 2.0.0 version
-  # cmk['server_rpm'] = "check-mk-free-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
+  cmk['server_rpm'] = "check-mk-free-#{cmk['cmk_release']}-el#{cmk['el_version']}-38.x86_64.rpm"
   cmk['agent_rpm'] = "check-mk-agent-#{cmk['cmk_release']}-1.noarch.rpm"
   cmk['site_name'] = 'cmk'
   cmk['server_name'] = 'checkmks'
