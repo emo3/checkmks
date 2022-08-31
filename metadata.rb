97d9035dc9455a64eb844804b7fa1104b190dbe4
Ed Overton <e.overton@gmail.com>
2022-08-30 23:08:41 -0400

    major bump

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'you1@example.com'
 chef_version     '>= 15.0'
 license          'Apache-2.0'
 description      'Installs/Configures checkmk server'
-version          '1.12.0'
+version          '2.0.0'
 
 supports 'redhat'
 supports 'centos'
