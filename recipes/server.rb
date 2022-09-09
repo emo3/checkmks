a4fe80d687d9c2c4d209fd7979dbf6e449a756ee
Ed Overton <e.overton@gmail.com>
2022-09-09 19:06:53 -0400

    add code to upgrade

--- recipes/server.rb
+++ recipes/server.rb
@@ -27,6 +27,15 @@ package node['cmk']['server_rpm'] do
   source "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_rpm']}"
 end
 
+# See if this is a upgrade
+execute "stop_#{node['cmk']['site_name']}" do
+  command "omd stop #{node['cmk']['site_name']}"
+  only_if "omd version|grep 'OMD - Open Monitoring Distribution Version #{node['cmk']['server_version']}'"
+  notifies :run, "execute[upgrade_#{node['cmk']['site_name']}]", :immediately
+  notifies :run, "execute[start_#{node['cmk']['site_name']}]", :immediately
+  action :nothing
+end
+
 execute "create_#{node['cmk']['site_name']}" do
   command "omd create --admin-password #{node['cmk']['admin_passwd']} #{node['cmk']['site_name']}"
   sensitive true
@@ -34,12 +43,40 @@ execute "create_#{node['cmk']['site_name']}" do
   notifies :run, "execute[start_#{node['cmk']['site_name']}]", :immediately
 end
 
+execute "upgrade_#{node['cmk']['site_name']}" do
+  command "omd setversion #{node['cmk']['cmk_release']}"
+  action :nothing
+end
+
+execute "upgrade_#{node['cmk']['site_name']}" do
+  command "omd start #{node['cmk']['site_name']}"
+  action :nothing
+end
+
 execute "start_#{node['cmk']['site_name']}" do
   command "omd start #{node['cmk']['site_name']}"
   action :nothing
 end
 
-cookbook_file '/tmp/cat_token.sh' do
-  source 'cat_token.sh'
-  mode '0755'
+# Create file from template
+template '/tmp/activate-checkmks.py' do
+  source 'activate-checkmks1.erb'
+  mode '0500'
+  sensitive false
+  variables(
+    cmkserver: 'localhost',
+    apitoken: lazy { `cat /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret`.chomp },
+    sitename: node['cmk']['site_name']
+  )
+  # notifies :run, 'execute[run_activate-checkmks]', :immediately
+end
+
+execute 'run_activate-checkmks' do
+  command '/tmp/activate-checkmks.py'
+  ignore_failure true
+  action :nothing
+end
+
+execute 'echo_token' do
+  command lazy { `cat /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret` }
 end
